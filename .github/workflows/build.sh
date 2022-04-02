990639af34cd38e91b2d78cbc8b3ba0ac6312c96
Thanh NGUYEN <thanh@uthng.me>
2022-04-02 22:58:31 +0200

    ci: fix docker login

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -6,6 +6,9 @@ RELEASE_VERSION="${1}"
 echo "[VERSION] Bump version"
 sed -i -e "s/^version = .*/version = \"${RELEASE_VERSION}\"/" Cargo.toml
 
+echo "[DOCKER] Login"
+echo "${DOCKERHUB_SECRET}" | docker login -u bazarlab --password-stdin
+
 if [[ "$GITHUB_REF" == 'refs/heads/master' || "$GITHUB_REF" == 'refs/heads/next' ]]; then
   echo "[RELEASE] Creating archives for different architectures"
   make archives-linux RELEASE_VERSION="${RELEASE_VERSION}"
--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -15,7 +15,7 @@ DATE="${DATE:-"$(date -u +%Y-%m-%d)"}"
 # PLATFORM for Buildx: linux/arm64 or linux/amd64
 PLATFORM="${PLATFORM:-"linux/amd64 linux/arm64"}"
 PUSH="${PUSH:-"true"}"
-REPO="${REPO:-"uthng/flowrunner"}"
+REPO="${REPO:-"bazarlab/flowrunner"}"
 DIR_ARTIFACTS="target/artifacts"
 
 #
